1faaae79f8c16d792cbc4d3d19465ef7c3b26892
William L <w-lindvall@outlook.com>
2020-05-11 23:46:34 -0700

    init

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const App = () => (
+  <>
+    <header className="header">
+      <h1>Conway's Checkboxes</h1>
+    </header>
+    <main className="main">hello world</main>
+  </>
+)
